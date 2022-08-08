d3e134f2d83ebdb6d533710da0d268e31e64f2d9
MusaAlsulami <109309832+MusaAlsulami@users.noreply.github.com>
2022-08-08 16:28:11 +0300

    Update dataset.php

--- dataset.php
+++ dataset.php
@@ -1,32 +1,4 @@
-Skip to content
-Search or jump to…
-Pull requests
-Issues
-Marketplace
-Explore
- 
-@MusaAlsulami 
-kira-Developer
-/
-php_analogwrite_to_db
-Public
-Code
-Issues
-Pull requests
-Actions
-Projects
-Wiki
-Security
-Insights
-php_analogwrite_to_db/index.php /
-@kira-Developer
-kira-Developer first commit
-Latest commit 25e8181 7 days ago
- History
- 1 contributor
-28 lines (21 sloc)  765 Bytes
 
-<?php
 require __DIR__ . "/inc/bootstrap.php";
  
 if (!file_exists("number.sqlite")) {require_once PROJECT_ROOT_PATH . "/inc/bulidDatabase.php";}
@@ -53,18 +25,3 @@ try{
 
 }
 
-?>
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
